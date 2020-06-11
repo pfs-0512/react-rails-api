0d131923c911031dbb4ba6a24c7213c84b843e25
katakawaryouhei <kyatakawa1230@gmail.com>
2020-06-11 20:18:44 +0900

    set front_end

--- crud_front/src/components/MainContainer.jsx
+++ crud_front/src/components/MainContainer.jsx
@@ -1,5 +1,8 @@
 import React from 'react'
 import axios from "axios"
+import ProductsContainer from './ProductContainer'
+import FormContainer from './FormContainer'
+import update from 'react-addons-update'
 
 class MainContainer extends React.Component {
   constructor(props) {
@@ -9,11 +12,47 @@ class MainContainer extends React.Component {
     }
   }
 
-componentDidMount() {
-    axios.get('http://localhost:3001/products')
-    .then((results) => {
-      console.log(results)
-      this.setState({products: results.data})
+  componentDidMount() {
+      axios.get('http://localhost:3001/products')
+      .then((results) => {
+        console.log(results)
+        this.setState({products: results.data})
+      })
+      .catch((data) =>{
+        console.log(data)
+      })
+    }
+
+  createProduct = (product) => {
+    axios.post('http://localhost:3001/products',{product: product} )
+    .then((response) => {
+      const newData = update(this.state.products, {$push:[response.data]})
+      this.setState({products: newData})
+    })
+    .catch((data) =>{
+      console.log(data)
+    })
+  }
+
+  deleateProduct = (id) => {
+    axios.delete(`http://localhost:3001/products/${id}`)
+    .then((response) => {
+      const productIndex = this.state.products.findIndex(x => x.id === id)
+      const deletedProducts = update(this.state.products, {$splice: [[productIndex, 1]]})
+      this.setState({products: deletedProducts})
+      console.log('set')
+    })
+    .catch((data) =>{
+      console.log(data)
+    })
+  }
+
+  updateProduct = (id, product) => {
+    axios.patch(`http://localhost:3001/products/${id}`,{product: product})
+    .then((response) => {
+      const productIndex = this.state.products.findIndex(x => x.id === id)
+      const products = update(this.state.products, {[productIndex]: {$set: response.data}})
+      this.setState({products: products})
     })
     .catch((data) =>{
       console.log(data)
@@ -23,6 +62,8 @@ componentDidMount() {
   render() {
     return (
       <div className='app-main'>
+        <FormContainer  hendleAdd={this.hendleAdd} createProduct={this.createProduct}/>
+        <ProductsContainer productData={this.state.products} deleateProduct={this.deleateProduct} updateProduct={this.updateProduct}/>
       </div>
     );
   }
